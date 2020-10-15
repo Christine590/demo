a9ea361ac56644fa255eb89c8472eb26728832ba
Christine <baba3425@gmail.com>
2020-10-15 09:26:22 +0800

    課程實作練習 Day1 進度

---
+++ angular_cub/src/app/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  sitename = 'hello world';
+  subtitle = '記載著 Will 在網路世界的學習心得與技術分享';
+  counter = 12;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // setTimeout(() => {
+    //   this.sitename = 'OK!!';
+    // }, 2000);
+  }
+
+  changeName($event: MouseEvent): void {
+    console.log($event);
+    this.counter++;
+
+    if ($event.ctrlKey) {
+      this.sitename = 'The Will Will Web';
+    }
+  }
+}
