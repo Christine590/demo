94f7eda66039813a6dab7ef7fa582f2fc3a1be27
Christine <baba3425@gmail.com>
2020-10-15 17:12:24 +0800

    使用 routerLink 與 Router 進行網頁導覽並傳入參數

--- startbootstrap-sb-admin-2-ng10/src/app/dashboard/dashboard.component.ts
+++ startbootstrap-sb-admin-2-ng10/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { chartAreaDemo } from '../chartAreaDemo';
 import { chartPieDemo } from '../chartPieDemo';
 
@@ -8,11 +9,25 @@ import { chartPieDemo } from '../chartPieDemo';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     chartAreaDemo();
     chartPieDemo();
+  }
+
+  goTo1(): void {
+    // navigate 是傳入陣列物件
+    this.router.navigate(['/utilities/color', 3], {
+      queryParams: {
+        name: 'Baiden'
+      }
+    });
+  }
+
+  goTo2(): void {
+    // navigateByUrl 是傳入純字串
+    this.router.navigateByUrl('/utilities/color/4?name=Trump');
   }
 
 }
