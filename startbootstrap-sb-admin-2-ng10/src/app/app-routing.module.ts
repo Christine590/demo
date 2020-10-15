12ae6526ea064e585bb916e758094dafda9ea94e
Christine <baba3425@gmail.com>
2020-10-15 16:02:07 +0800

    子路由設定

--- startbootstrap-sb-admin-2-ng10/src/app/app-routing.module.ts
+++ startbootstrap-sb-admin-2-ng10/src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { ColorComponent } from './utilities/color/color.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' }, // Match還有'prefix'可用，前綴詞相符
   { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
-  { path: '**', component: NotFoundComponent }
+  {
+    path: 'utilities',
+    // component: component, 用不到
+    children: [
+      { path: 'color', component: ColorComponent }
+    ]
+  },
+  { path: '**', component: NotFoundComponent } // 套用於全部
 ];
 
 @NgModule({
