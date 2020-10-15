8d6aa7749bf129b978cb78285b40cfa03c8b6628
Christine <baba3425@gmail.com>
2020-10-15 11:13:07 +0800

    自訂 ccmask Pipe 元件與使用 json Pipe 元件

--- angular_cub/src/app/ccmask.pipe.ts
+++ angular_cub/src/app/ccmask.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CcmaskPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string { // 傳入string 輸出string
+    const re = /(\d{4})-(\d{4})-(\d{4})-(\d{4})/; // 正則表達式
+    return value.replace(re, '$1-****-****-$4'); // 傳回隱碼的版本，but前端還是撈的到
   }
-
 }
