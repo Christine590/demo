9147c256e7b774c4aa8b1f6685d871e940aaf8e6
Christine <baba3425@gmail.com>
2020-10-17 17:08:28 +0800

    重新建立UtilititesModule 並移除預設建立的 UtilitiesComponent 元件

--- startbootstrap-sb-admin-2-ng10/src/app/app-routing.module.ts
+++ startbootstrap-sb-admin-2-ng10/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
+  { path: 'utilities', loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule) },
   { path: '**', component: NotFoundComponent } // 套用於全部
 ];
 
