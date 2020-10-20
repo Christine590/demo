0ad60cfa8b749930652c6f2894588a5bfd9936b4
Christine <baba3425@gmail.com>
2020-10-20 10:12:51 +0800

    加入 login 元件

--- startbootstrap-sb-admin-2-ng10/src/app/app-routing.module.ts
+++ startbootstrap-sb-admin-2-ng10/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' }, // Match還有'prefix'可用，前綴詞相符
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
   { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
