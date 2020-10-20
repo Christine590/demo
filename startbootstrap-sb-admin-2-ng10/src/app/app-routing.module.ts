7bf8063bf3d34044ff020ac98812609e57a3abb5
Christine <baba3425@gmail.com>
2020-10-20 10:12:29 +0800

    加入 login 元件

--- startbootstrap-sb-admin-2-ng10/src/app/app-routing.module.ts
+++ startbootstrap-sb-admin-2-ng10/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
+import { LoginComponent } from './login/login/login.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { Page1Component } from './page1/page1.component';
@@ -7,6 +8,7 @@ import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' }, // Match還有'prefix'可用，前綴詞相符
+  { path: 'login', component: LoginComponent}
   { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
@@ -17,7 +19,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true // 瀏覽器就不會視為整個換頁
+    useHash: true, // 瀏覽器就不會視為整個換頁
+    preloadingStrategy: PreloadAllModules
     })],
   exports: [RouterModule]
 })
