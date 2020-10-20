355b301e7af940d4e2c19080a4225a647c55cdc0
Christine <baba3425@gmail.com>
2020-10-20 10:28:55 +0800

    動態設定進入 Login 頁面時的 body class 內容

--- startbootstrap-sb-admin-2-ng10/src/app/login/login/login.component.ts
+++ startbootstrap-sb-admin-2-ng10/src/app/login/login/login.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+
+  existingBodyClassName = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    this.existingBodyClassName = document.body.className;
+    document.body.className = 'bg-gradient-primary'; // 進入此頁時新增自訂class
   }
 
+  ngOnDestroy(): void {
+    document.body.className = this.existingBodyClassName; // 離開此頁時移除class
+  }
 }
