93bfbb22e48a5b31d7bc6417674f3db5630e3139
Christine <baba3425@gmail.com>
2020-10-20 11:10:33 +0800

    完成權限不足時自動導向到登入頁，登入成功後會回到原本要去的頁面

--- startbootstrap-sb-admin-2-ng10/src/app/auth.guard.ts
+++ startbootstrap-sb-admin-2-ng10/src/app/auth.guard.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router) { } // 使用Router服務元件要先DI(注入)
+
   canActivate(
     next: ActivatedRouteSnapshot,
+    // state支援4種型別的回傳
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (localStorage.getItem('token')) {
+      return true;
+    } else {
+      // return this.router.parseUrl('/login'); // 直接return false也可以，只是會像壞掉因為點擊沒反應，所以改為轉導login頁
+        return this.router.parseUrl('/login?returnurl=' + state.url); // 帶上點擊頁面的URL
+    }
   }
 }
--- startbootstrap-sb-admin-2-ng10/src/app/login/login/login.component.ts
+++ startbootstrap-sb-admin-2-ng10/src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
@@ -8,7 +9,7 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   existingBodyClassName = '';
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { } // 使用Router服務元件要先DI(注入)
 
   ngOnInit(): void {
     this.existingBodyClassName = document.body.className;
@@ -17,5 +18,10 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     document.body.className = this.existingBodyClassName; // 離開此頁時移除class
+  }
+
+  doLogin() {
+    localStorage.setItem('token', '123123123213'); // 預設token以進行登入驗證的測試
+    this.router.navigateByUrl(this.route.snapshot.queryParamMap.get('returnUrl')); // 取得當前URL所帶的returnUrl參數
   }
 }
