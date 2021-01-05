f8cf1f4d8101464be5c4f834b02d0220dfbfb384
Christine <baba3425@gmail.com>
2021-01-05 21:17:39 +0800

    加入HomeController並將資料顯示於view

---
+++ ASP.NET MVC/slnFirstMvc/prjFirstMvc/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjFirstMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "進站時間:"+DateTime.Now.ToString();
+        }
+        public ActionResult ShowTime() // 在Action右鍵>新增檢視(View)，會比照controller名稱將新增的view分類
+        {
+            ViewBag.DateTime = DateTime.Now.ToString(); // 存在ViewBag的動態屬性DateTime裡面
+            return View();
+        }
+    }
+}
