8746c51ba708a6e1cb477ed5cd26dff1dcc41882
Christine <baba3425@gmail.com>
2021-01-05 21:46:05 +0800

    將資料以Model方式嵌入cshtml以取得資料

--- ASP.NET MVC/slnFirstMvc/prjFirstMvc/Controllers/HomeController.cs
+++ ASP.NET MVC/slnFirstMvc/prjFirstMvc/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using prjFirstMvc.Models;
 
 namespace prjFirstMvc.Controllers
 {
@@ -13,10 +14,20 @@ namespace prjFirstMvc.Controllers
         {
             return "進站時間:"+DateTime.Now.ToString();
         }
-        public ActionResult ShowTime() // 在Action右鍵>新增檢視(View)，會比照controller名稱將新增的view分類
+        public ActionResult ShowTime() // 在Action右鍵>新增檢視(View)，會比照controller名稱將新增的view分類，勾選版面配置會自動套Bootstrap
         {
             ViewBag.DateTime = DateTime.Now.ToString(); // 存在ViewBag的動態屬性DateTime裡面
             return View();
+        }
+        public ActionResult ShowEmp()
+        {
+            CEmployee[] emps = new CEmployee[]
+            {
+                new CEmployee(){  fId=1, fName="吳小瓜", fSalary=40000},
+                new CEmployee(){  fId=2, fName="林阿呆", fSalary=50000},
+                new CEmployee(){  fId=3, fName="洪通通", fSalary=45000}
+            };
+            return View(emps.ToList()); // 把資料丟回View去顯示
         }
     }
 }
---
+++ ASP.NET MVC/slnFirstMvc/prjFirstMvc/Models/CEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjFirstMvc.Models
+{
+    public class CEmployee
+    {
+        public int fId { get; set; }
+        public string fName { get; set; }
+        public int fSalary { get; set; }
+    }
+}
