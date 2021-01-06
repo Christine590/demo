f7f5f1b99c80821410e5ff043fa3e084d164cf29
Christine <baba3425@gmail.com>
2021-01-06 21:08:35 +0800

    練習DB資料新增

--- ASP.NET MVC/slnEmps/prjEmps/Controllers/HomeController.cs
+++ ASP.NET MVC/slnEmps/prjEmps/Controllers/HomeController.cs
@@ -17,5 +17,26 @@ namespace prjEmps.Controllers
             var emps = db.tEmployee.OrderByDescending(m => m.fId).ToList();
             return View(emps); //將emps傳回view
         }
+
+        // 透過打網址的 GET 方式
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string fName, string fPhone, int fSalary) // 傳入前端form裡對應的input name
+        {
+            tEmployee emp = new tEmployee();
+            emp.fName = fName;
+            emp.fPhone = fPhone;
+            emp.fSalary = fSalary;
+
+            db.tEmployee.Add(emp); // 新增資料到DB物件
+            db.SaveChanges(); // 儲存資料庫變更
+            return RedirectToAction("Index"); // 重新執行 Index Action
+        }
+
+
     }
 }
