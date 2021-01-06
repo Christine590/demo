171451d2afc2101172a927534e378e84d986f382
Christine <baba3425@gmail.com>
2021-01-06 20:49:48 +0800

    將tEmployee資料表降冪排列並傳回view顯示

---
+++ ASP.NET MVC/slnEmps/prjEmps/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prjEmps.Models;
+
+namespace prjEmps.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbEmpEntities db = new dbEmpEntities(); // new一個資料庫物件來使用
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var emps = db.tEmployee.OrderByDescending(m => m.fId).ToList();
+            return View(emps); //將emps傳回view
+        }
+    }
+}
