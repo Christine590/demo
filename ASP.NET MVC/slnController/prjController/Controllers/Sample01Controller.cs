3b5f4a1f4c814a58ed6725ccacaac885147eb245
Christine <baba3425@gmail.com>
2021-01-08 22:30:07 +0800

    用POST傳入表單資料並存入ViewBag

---
+++ ASP.NET MVC/slnController/prjController/Controllers/Sample01Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjController.Controllers
+{
+    public class Sample01Controller : Controller
+    {
+        // GET: Sample01
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string PId, string PName, int Price)
+        {
+            // 將傳入的值存進 ViewBag
+            ViewBag.PId = PId;
+            ViewBag.PName = PName;
+            ViewBag.Price = Price;
+
+            return View();
+        }
+    }
+}
