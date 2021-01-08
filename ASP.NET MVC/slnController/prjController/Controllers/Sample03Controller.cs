2115d055f0ee94938c60fc5b3d5ec37ccdbb6f94
Christine <baba3425@gmail.com>
2021-01-08 22:44:43 +0800

    使用FormCollection取得表單欄位控制項的值

---
+++ ASP.NET MVC/slnController/prjController/Controllers/Sample03Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjController.Controllers
+{
+    public class Sample03Controller : Controller
+    {
+        // GET: Sample03
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection forms)
+        {
+            ViewBag.PId = forms["PId"]; // forms[0] 意指表單的第一個欄位，以此例來看意思相同
+            ViewBag.PName = forms["PName"]; // 對應的key拼錯字就GG取不到，似以前的request.form用法
+            ViewBag.Price = forms["Price"];
+            return View();
+        }
+    }
+}
