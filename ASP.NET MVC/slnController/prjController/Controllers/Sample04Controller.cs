51aad4c92ef61e49e7d928d062f29dcee4bae805
Christine <baba3425@gmail.com>
2021-01-08 22:54:14 +0800

    利用Model繫結將資料綁定至前端畫面顯示

---
+++ ASP.NET MVC/slnController/prjController/Controllers/Sample04Controller.cs
@@ -0,0 +1,22 @@
+﻿using prjController.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjController.Controllers
+{
+    public class Sample04Controller : Controller
+    {
+        // GET: Sample04
+        public ActionResult ShowProduct()
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product() { PId = "A01", PName = "XBox One", Price = 12800 });
+            list.Add(new Product() { PId = "A02", PName = "PS 4", Price = 9500 });
+            list.Add(new Product() { PId = "A03", PName = "Switch", Price = 14500 });
+            return View(list);
+        }
+    }
+}
