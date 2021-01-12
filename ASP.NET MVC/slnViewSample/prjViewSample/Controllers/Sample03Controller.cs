131ef2aa148d62b65694c234ceba03491b220f37
Christine <baba3425@gmail.com>
2021-01-12 22:22:50 +0800

    將下載的Bootstrap跟JQuery檔案置入專案並新增實作用的controller與view

---
+++ ASP.NET MVC/slnViewSample/prjViewSample/Controllers/Sample03Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjViewSample.Controllers
+{
+    public class Sample03Controller : Controller
+    {
+        // GET: Sample03
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
