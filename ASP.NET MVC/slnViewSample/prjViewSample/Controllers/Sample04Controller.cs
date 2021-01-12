f840fd3833ad5293918b3e20f54dcb8249ffc205
Christine <baba3425@gmail.com>
2021-01-12 22:39:00 +0800

    使用Bootstrap樣式美化影片播放頁

---
+++ ASP.NET MVC/slnViewSample/prjViewSample/Controllers/Sample04Controller.cs
@@ -0,0 +1,24 @@
+﻿using prjViewSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjViewSample.Controllers
+{
+    public class Sample04Controller : Controller
+    {
+        // GET: Sample04
+        public ActionResult Index(int n = 0)
+        {
+            List<Movie> list = new List<Movie>();
+            list.Add(new Movie() { Id = "GhwVSzJZoHk", Name = "ASP NET MVC待辦事項CRUD(一)" });
+            list.Add(new Movie() { Id = "Q8zfvr9gxUs", Name = "ASP NET MVC待辦事項CRUD(二)" });
+            list.Add(new Movie() { Id = "j5mlxarc5-g", Name = "ASP.NET MVC 控制器與動作方法" });
+            list.Add(new Movie() { Id = "li9sMIdNEho", Name = "ASP.NET MVC-模型繫結" });
+            ViewBag.MovieId = list[n].Id;
+            return View(list);
+        }
+    }
+}
