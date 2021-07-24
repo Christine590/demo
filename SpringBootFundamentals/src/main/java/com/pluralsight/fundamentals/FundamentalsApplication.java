65bc6ee4523276fef86c61143415efd0caa5a0ba
Christine590 <baba3425@gmail.com>
2021-07-24 16:47:37 +0800

    加入debug設定

--- SpringBootFundamentals/src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
+++ SpringBootFundamentals/src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// includes @SpringBootConfiguration, @EnableAutoConfiguration, @ComponentScan
 public class FundamentalsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentalsApplication.class, args);
+		System.out.println("Hello Pluralsight!");
 	}
 
 }
