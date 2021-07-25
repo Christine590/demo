bc74b52946ed746f2ae1361460e465efe2cc6b60
Christine590 <baba3425@gmail.com>
2021-07-25 09:18:05 +0800

    新增註解

--- SpringBootFundamentals/Module3/fundamentals/src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
+++ SpringBootFundamentals/Module3/fundamentals/src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//includes @SpringBootConfiguration, @EnableAutoConfiguration, @ComponentScan
 public class FundamentalsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentalsApplication.class, args);
